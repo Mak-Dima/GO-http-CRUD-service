f713f7f2aa1798a783dcfc41b7ad5547855dfec0
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2025-12-28 14:12:55 -0600

    Foundation for CRUD handlers added.

--- main.go
+++ main.go
@@ -21,5 +21,25 @@ func main() {
 		w.Write([]byte("Hello, World!"))
 	})
 
+	srvMux.HandleFunc("/read", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("This is the read endpoint."))
+	})
+
+	srvMux.HandleFunc("/write", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("This is the write endpoint."))
+	})
+
+	srvMux.HandleFunc("/update", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("This is update endpoint."))
+	})
+
+	srvMux.HandleFunc("/delete", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("This is delete endpoint."))
+	})
+
 	log.Fatal(server.ListenAndServe())
 }
